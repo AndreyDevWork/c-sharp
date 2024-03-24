66b13c68aebeea96271d839348a2e2e054767cc6
Andrey <andrey.develop.a@gmail.com>
2024-03-24 13:26:32 +0700

    feat: data types

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace c_sharp
     {
         public static void Main()
         {
-            Console.WriteLine("Hello, World");
+            int integerVariable = 10;
+            string stringVariable = "Hello, World";
+            double doubleVariable = 1.5;
+            bool boolVariable = true;
+
+            if (boolVariable)
+            {
+                doubleVariable *= integerVariable;
+                stringVariable += " " + doubleVariable;
+            }
+
+            object[] mixedArray = new object[] { "one", 11, "two", 3.14, "END ARRAY:(" };
+
+            foreach (var item in mixedArray)
+            {
+                Console.Write(item);
+            }
+
+            Console.WriteLine(stringVariable + ' ' + integerVariable * doubleVariable + boolVariable);
         }
     }
 }
